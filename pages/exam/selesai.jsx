00b7ae779c9afff635c183ee00036d781b80fa73
akrindev <akrinmin@gmail.com>
2021-12-02 11:16:06 +0700

    added ended exam

--- pages/exam/selesai.jsx
+++ pages/exam/selesai.jsx
@@ -2,6 +2,7 @@ import ExamBegin from "../../components/Layouts/ExamBegin"
 // import lottie
 import Lottie from "react-lottie";
 import data from "../../assets/successful.json"
+import { useRouter } from "next/router";
 
 function Header() {
     return (
@@ -12,6 +13,8 @@ function Header() {
 }
 
 export default function Selesai() {
+    const router = useRouter()
+
     const defaultOptions = {
         loop: true,
         autoplay: true,
@@ -24,15 +27,20 @@ export default function Selesai() {
     return (
         <>
             <ExamBegin header={<Header />}>
-                <div className="relative flex items-center justify-center">
-                    <div className="bg-white rounded-md shadow p-5">
+                <div className="relative flex items-center justify-center w-full">
+                    <div className="bg-white rounded-md shadow w-full max-w-md p-5">
                         <Lottie options={defaultOptions} height={200} width={200} />
 
-                        <div className="flex items-center justify-center">
+                        <div className="flex flex-col items-center justify-center mt-5">
+                            <div className="text-xl font-extrabold font-nunito">Selesai</div>
 
-                            <div className="text-lg font-bold">Selesai</div>
+                            <p className="font-poppins mt-3">Terima kasih telah menyelesaikan ujian</p>
 
-                            <p>Terima kasih telah menyelesaikan ujian</p>
+                            <div className="mt-5">
+                                <button onClick={() => router.push('/dashboard')} className="bg-gradient-to-r from-lightBlue-500 to-lightBlue-400 w-full py-3 px-6 rounded-md shadow text-white font-poppins">
+                                    Kembali ke dashboard
+                                </button>
+                            </div>
                         </div>
 
                     </div>
