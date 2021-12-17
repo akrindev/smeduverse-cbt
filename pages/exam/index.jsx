8e3dab5ccad50c430c757f38f806dc64278535e8
akrindev <akrinmin@gmail.com>
2021-12-17 12:57:36 +0700

    fixed

--- pages/exam/index.jsx
+++ pages/exam/index.jsx
@@ -14,9 +14,8 @@ import QuestionOption from "../../components/QuestionOption"
 import NavButirSoal from '../../components/Exam/NavButirSoal';
 import Modal from '../../components/Dialog'
 
-function NavHead({ dateEnd }) {
+function NavHead({ dateEnd, mapel, tingkatKelas }) {
 
-    
     let hour = Math.floor((dateEnd / 3600000) % 24); // time diff's hours (modulated to 24)
     let min = Math.floor((dateEnd / 60000) % 60); // time diff's minutes (modulated to 60)
     let sec = Math.floor((dateEnd / 1000) % 60); // time diff's seconds (modulated to 60)
@@ -32,8 +31,9 @@ function NavHead({ dateEnd }) {
                         sisa waktu: {`${hour}j ${min}m ${sec}d`}
                     </div>
                 </div>
-                <div className="bg-white px-3 py-2 mt-3 -mb-6 rounded shadow">
-                    hellow
+                <div className="flex flex-col bg-white px-3 py-2 mt-3 -mb-6 rounded shadow">
+                    <h2 className="text-lg">{mapel}</h2>
+                    <div className='text-sm text-gray-500'>Kelas {tingkatKelas}</div>
                 </div>
             </div>
         </>
@@ -52,9 +52,9 @@ export default function ExamIndex() {
     const [endTime, setEndTime] = useState(null)
     // dialog state
     const [isOpenDialog, setIsOpenDialog] = useState(false)
-   
-    if(!questions && !savedAnswers) {
-        if(typeof window !== "undefined") {
+
+    if (!questions && !savedAnswers) {
+        if (typeof window !== "undefined") {
             window.location.href = '/dashboard'
         }
     }
@@ -76,7 +76,7 @@ export default function ExamIndex() {
             setSavedAnswers(savedAnswers => {
                 const newSavedAnswers = _.find(savedAnswers, (item) => item.exam_soal_id === question.id)
                 newSavedAnswers.answer_chosen_id = value
-    
+
                 return savedAnswers
             })
         }).catch((err) => console.log(err)).finally(() => {
@@ -104,15 +104,15 @@ export default function ExamIndex() {
         }
     }, [warn])
 
-    
+
     useEffect(() => {
         const getChosenAnswer = () => {
             const chosen = _.find(savedAnswers, { exam_soal_id: question?.id })
-    
+
             setChosenAnswer(chosen)
         }
 
-        if(questions && questions.length > 0) {            
+        if (questions && questions.length > 0) {
             setQuestion(questions[questionIndex])
             getChosenAnswer()
         }
@@ -123,15 +123,15 @@ export default function ExamIndex() {
             setEndTime(prev => time)
         }, 1000)
     }, [])
-    
+
     useEffect(() => {
         const getChosenAnswer = () => {
             const chosen = _.find(savedAnswers, { exam_soal_id: question?.id })
-    
+
             setChosenAnswer(chosen)
         }
-        
-        if(questions && questions.length > 0) {            
+
+        if (questions && questions.length > 0) {
             setQuestion(questions[questionIndex])
             getChosenAnswer()
         }
@@ -150,7 +150,7 @@ export default function ExamIndex() {
             <Head>
                 <title>Ujian</title>
             </Head>
-            <ExamBegin header={<NavHead dateEnd={endTime} />}>
+            <ExamBegin header={<NavHead dateEnd={endTime} mapel={examInfo.mapel} tingkatKelas={examInfo.tingkat} />}>
 
                 <div className="relative my-3 w-full max-w-7xl mx-auto">
                     <div className="grid grid-cols-12 gap-6 w-full mx-auto">
@@ -160,13 +160,13 @@ export default function ExamIndex() {
                                 <div className="px-5 py-3 border-b border-gray-200 font-nunito font-semibold text-xl">
                                     Pertanyaan {questionIndex + 1}
                                 </div>
-                                {/* pertanyaan */}  
+                                {/* pertanyaan */}
                                 <div className="px-4 py-3 font-nunito font-normal text-base" dangerouslySetInnerHTML={dangerHTML()} />
 
                                 <div className="border-b border-gray-100"></div>
 
                                 <div className="p-3">
-                                    <QuestionOption data={question.choices} chosen={chosenAnswer} onChosen={handleChosen} isSaving={isSaving}/>
+                                    <QuestionOption data={question.choices} chosen={chosenAnswer} onChosen={handleChosen} isSaving={isSaving} />
                                 </div>
 
                                 <div className="p-3 pb-5">
@@ -187,7 +187,7 @@ export default function ExamIndex() {
 
                         </div>
                         <div className="col-span-12 lg:col-span-4">
-                            
+
                             <div className="p-4 bg-white rounded shadow">
                                 <div className="font-bold text-base mb-5">Navigasi Soal</div>
                                 <div className="grid grid-cols-10 gap-2 w-full mx-auto">
@@ -196,30 +196,30 @@ export default function ExamIndex() {
                                             isChosen={isChosen(question.id)}
                                             onClick={() => setQuestionIndex(index)}
                                             key={question.id}
-                                            number={index+1}/>)
+                                            number={index + 1} />)
                                     )}
                                 </div>
                                 <div className="mt-5 flex justify-end">
                                     <button
                                         className="bg-gray-200 text-sm px-4 py-1 rounded-lg border border-gray-500 font-medium"
                                         onClick={stopExam}
-                                        >
-                                            Hentikan Ujian
+                                    >
+                                        Hentikan Ujian
                                     </button>
                                 </div>
                                 <div className="mt-3">
                                     <div className="flex flex-col items-start">
                                         <div className="flex items-center space-x-3">
-                                            <div className="h-2 w-2 bg-green-500 rounded"></div> <span className="text-xs text-gray-600">soal telah dijawab</span> 
+                                            <div className="h-2 w-2 bg-green-500 rounded"></div> <span className="text-xs text-gray-600">soal telah dijawab</span>
                                         </div>
                                         <div className="flex items-center space-x-3 mt-1">
-                                            <div className="h-2 w-2 bg-yellow-500 rounded"></div> <span className="text-xs text-gray-600">ragu-ragu</span> 
+                                            <div className="h-2 w-2 bg-yellow-500 rounded"></div> <span className="text-xs text-gray-600">ragu-ragu</span>
                                         </div>
                                         <div className="flex items-center space-x-3 mt-1">
-                                            <div className="h-2 w-2 bg-gray-700 rounded"></div> <span className="text-xs text-gray-600">belum dijawab</span> 
+                                            <div className="h-2 w-2 bg-gray-700 rounded"></div> <span className="text-xs text-gray-600">belum dijawab</span>
                                         </div>
                                         <div className="flex items-center mt-5 text-xs font-medium">
-                                            <div className="text-red-600 mr-1">Peringatan</div> <span className="text-xs text-red-900 font-bold"> {warn} </span> 
+                                            <div className="text-red-600 mr-1">Peringatan</div> <span className="text-xs text-red-900 font-bold"> {warn} </span>
                                         </div>
                                     </div>
                                 </div>
@@ -232,7 +232,7 @@ export default function ExamIndex() {
                 </div>
                 <ToastContainer />
             </ExamBegin>
-            <Modal isOpen={isOpenDialog} setIsOpen={setIsOpenDialog} title={`Yakin udah selesai?`} description={<ChosenAnswer answers={savedAnswers}/>} action={<ButtonResult sheetId={savedAnswers && savedAnswers[0]?.exam_answer_sheet_id} />}/>
+            <Modal isOpen={isOpenDialog} setIsOpen={setIsOpenDialog} title={`Yakin udah selesai?`} description={<ChosenAnswer answers={savedAnswers} />} action={<ButtonResult sheetId={savedAnswers && savedAnswers[0]?.exam_answer_sheet_id} />} />
         </>
     )
 }
@@ -246,8 +246,8 @@ function ButtonResult({ sheetId }) {
         await api.post('/api/exam/get-result', {
             sheet_id: sheetId
         }).then(res => {
-           if(res.status == 200) {
-               toast.success('Lembar Ujian berhasil di kumpulkan')
+            if (res.status == 200) {
+                toast.success('Lembar Ujian berhasil di kumpulkan')
                 //    remove local storaage exam and answer
                 setTimeout(() => {
                     if (typeof window !== "undefined") {
@@ -256,9 +256,9 @@ function ButtonResult({ sheetId }) {
                         router.push('/exam/selesai')
                     }
                 }, 1200)
-           } 
+            }
         })
-        .catch(err => console.error(err)).finally(() => setIsLoading(false))
+            .catch(err => console.error(err)).finally(() => setIsLoading(false))
     }
 
     return (<>
@@ -270,11 +270,11 @@ function ButtonResult({ sheetId }) {
 }
 
 function ChosenAnswer({ answers }) {
-    
+
     // get chosen answer where answer_chosen_id is not null
     const chosenAnswer = _.filter(answers, (answer) => answer.answer_chosen_id != null).length || 0
     const emptyAnswer = _.filter(answers, (answer) => answer.answer_chosen_id == null).length || 0
-    
+
     return (
         <>
             <div>
