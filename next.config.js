0a41ef7c53f235e6774b3591c73dac81e74bdaba
akrindev <akrinmin@gmail.com>
2022-01-11 10:22:33 +0700

    fixed exportPathMap

--- next.config.js
+++ next.config.js
@@ -4,5 +4,16 @@ module.exports = {
   // image loader
   images: {
     loader: 'custom'
-  }
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/dashboard': { page: '/dashboard' },
+      '/exam': { page: '/exam' },
+      '/exam/selesai': { page: '/exam/selesai' },
+    }
+  },
 }
