0aec41813b85b478b560a1a04c22b6c3ade9e53b
akrindev <akrinmin@gmail.com>
2022-02-26 05:47:10 +0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import 'tailwindcss/tailwind.css'
 import 'react-toastify/dist/ReactToastify.min.css';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GWKJDQTDRW"
+        strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-GWKJDQTDRW');
+        `}
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
